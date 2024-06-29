109bdf50ed31045c6bd843b2c4365496dc35425f
Maxim_B <benko_0287@mail.ru>
2024-06-29 13:05:14 +0300

    Билд

--- src/components/workBlock/navbar/ItemNavbar.js
+++ src/components/workBlock/navbar/ItemNavbar.js
@@ -3,7 +3,6 @@ import iconKubik from './icons/iconKubik.png'
 import './itemNavbar.css'
 
 const ItemNavbar = (props) => {
-      console.log(props.text);
       return (
             <div className='navbar__item'>
                   <img className='navbar__item-icon' src={iconKubik} alt="" />
